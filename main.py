bac94aab70177b982db345615d8c6a0f5d1bf32d
XiuRen Admin <hoenaanka@gmail.com>
2026-03-03 22:50:58 +0200

    Folders
    Store meta folders for later

--- main.py
+++ main.py
@@ -11,3 +11,14 @@ with open('.data.json') as data_json:
     for meta_folder_name, meta_folder_path in xr_db['meta_folders'].items():
         os.makedirs(meta_folder_path, exist_ok=True)
         meta_paths[meta_folder_name] = os.path.abspath(meta_folder_path)
+
+    root = meta_paths["all"]
+    models = meta_paths["models"]
+    unknown_models = meta_paths["unknown_models"]
+    photographers = meta_paths["photographers"]
+    unknown_photographers = meta_paths["unknown_photographers"]
+    specials = meta_paths["specials"]
+    collections = meta_paths["collections"]
+    crossovers = meta_paths["crossovers"]
+    featuring = meta_paths["featuring"]
+    free = meta_paths["free"]
