77f239c8fcd1fbb0892e84d9255267bfb11c991f
XiuRen Admin <hoenaanka@gmail.com>
2026-03-11 10:37:40 +0200

    Folders
    Create personal collection of favourite models

--- main.py
+++ main.py
@@ -230,4 +230,26 @@ with winshell.shortcut(bol_17_issue_path) as shortcut:
     shortcut.write(os.path.join(bol_17_model_path, bol_17_stamp+LNK))
 os.utime(os.path.join(bol_17_model_path, bol_17_stamp+LNK), (bol_17_release_date_unix, bol_17_release_date_unix))
 
+# Personal Links to models
+for model in other:
+    link_to_model = os.path.join(models, model)
+    link_to_collection = os.path.join(other_path, model+LNK)
+    print(f"Linking {link_to_model} to {link_to_collection}")
+    with winshell.shortcut(link_to_model) as shortcut:
+        shortcut.write(link_to_collection)
+
+for model in various:
+    link_to_model = os.path.join(models, model)
+    link_to_collection = os.path.join(various_path, model+LNK)
+    print(f"Linking {link_to_model} to {link_to_collection}")
+    with winshell.shortcut(link_to_model) as shortcut:
+        shortcut.write(link_to_collection)
+
+for model in main:
+    link_to_model = os.path.join(models, model)
+    link_to_collection = model+LNK # this folder
+    print(f"Linking {link_to_model} to {link_to_collection}")
+    with winshell.shortcut(link_to_model) as shortcut:
+        shortcut.write(link_to_collection)
+
 input("Finished.")
